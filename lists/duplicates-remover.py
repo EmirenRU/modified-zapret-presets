6436e6702b49008ae61c2f5670f046a8c58a8082
Emil Yusupov <mrjava3300@mail.ru>
2025-06-20 23:31:45 +0300

    feat(Emiren - main): added the python script for removing duplicate lines

---
+++ lists/duplicates-remover.py
@@ -0,0 +1,27 @@
+import os
+
+def get_lists_of_files(directory):
+    txt_files = []
+    for filename in os.listdir(directory):
+        if filename.endswith('.txt'):
+            txt_files.append(os.path.join(directory, filename))
+    return txt_files
+
+def remove_duplicate_linse(f):
+    seen = set()
+
+    with open(f, 'r') as file:
+        for line in file:
+            sl = line.strip()
+            if sl not in seen:
+                seen.add(sl)
+    
+    with open(f, 'w') as file:
+        for line in seen:
+            file.write(line + '\n')
+
+
+if __name__ == "__main__":
+    files = get_lists_of_files(os.getcwd())
+    for file in files:
+        remove_duplicate_linse(file)
